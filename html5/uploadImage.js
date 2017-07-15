19d7fee9b6770d2e41bac797b98e829035d769bf
liuml <605880092@qq.com>
2017-07-15 13:04:11 +0800

    图片裁剪上传

--- html5/uploadImage.js
+++ html5/uploadImage.js
@@ -1,14 +1,28 @@
 (function(){
-	document.querySelector('.logo').addEventListener('click',function(){
-		document.querySelector('input[type="file"]').click();
+	$('.logo').on('click',function(){
+		$('input[type="file"]').click();
 	});
-	document.querySelector('input[type="file"]').addEventListener('change',function(){
+	$('input[type="file"]').on('change',function(){
 		fileSelected();
 	});
-	var maxFilesize = 1024*1024*2;	//2M
+	$('.cancel').on('click', function() {
+	    $("#showEdit").fadeOut();
+	    $('#showResult').fadeIn();
+	});
+
+	$('.confirm').on('click', function() {
+	    $("#showEdit").fadeOut();
+	    var $image = $('#report > img');
+	    var dataURL = $image.cropper("getCroppedCanvas");
+	    var imgurl = dataURL.toDataURL("image/jpeg", 0.5);
+	    $(".logo > img").attr("src", imgurl);
+	    $('#showResult').fadeIn();
+
+	});
+	var maxSize = 1024*1024*3;	//3M
 	function fileSelected(){
 		// 读取文件
-		var file = document.querySelector('input[type="file"]').files[0];
+		var file = $('input[type="file"]')[0].files[0];
 		var regFile = /^(image\/bmp|image\/gif|image\/jpeg|image\/png|image\/tiff)$/i;
 		if (!regFile.test(file.type)) {
 			alert('文件格式必须为图片');
@@ -19,43 +33,44 @@
 			canvas = document.createElement('canvas'),
 			ctx = canvas.getContext('2d');
 		// 文件加载完成
-		reader.addEventListener('load',function(){
+		$(reader).on('load',function(){
 			var url = this.result;
 			image.src = url;
 		});
 		// 图片加载完成
-		image.addEventListener('load',function(){
+		$(image).on('load',function(){
 			var w = image.naturalWidth,
 				h = image.naturalHeight,
-				scale = 3;	//图片缩放比例，此处是把图片大小高宽均缩小3倍
+				scale = 1,	//图片缩放比例
+				quality = 0.5;
+			scale = maxSize > file.size ? 1 : 10;
+			quality = maxSize > file.size ? 0.8: 0.5;
 			canvas.width = w/scale;
 			canvas.height = h/scale;
 			ctx.drawImage(image,0,0,w,h,0,0,canvas.width,canvas.height);
-			//上传文件
-			fileUpload();
+			var data = canvas.toDataURL('image/jpeg',quality);
+			$('#report').html('<img src="' + data + '" style="width: 100%;height:100%">');
+			cutImage();
 		});
 		//用文件加载器加载文件
 		reader.readAsDataURL(file);  
 		
-		function fileUpload(){
-			var quality = 0.3;	//图片的质量
-			var data = canvas.toDataURL('image/jpeg',quality);
-			//调用ajax上传图片base64数据
-			// 设置logo
-			document.querySelector('.logo img').src = data;
-			
-			// 转为Blob对象
-			// data = data.split(',')[1];
-			// data = window.atob(data);
-			// var ia = new Uint8Array(data.length);
-			// for(var i = 0; i < data.length;i++){
-			// 	ia[i] = data.charCodeAt(i);
-			// }
-			// var blob = new Blob([ia],{
-			// 	type:'image/jpeg'
-			// });
-			// console.log(blob);
-		}
-
+	}
+	function cutImage(){
+		$('#showResult').fadeOut();
+        $("#showEdit").fadeIn();
+        var $image = $('#report > img');
+        $image.cropper({
+            aspectRatio: 1 / 1,
+            viewMode:1,
+           	dragMode:'move',
+            guides: false,
+            highlight: false,
+            cropBoxMovable: false,
+            cropBoxResizable: false,
+            background: false,
+            minContainerHeight: 400,
+            minContainerWidth: 300
+        });
 	}
 })();
