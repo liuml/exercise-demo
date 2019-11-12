30e5ef7d76acfb0fdd574995db43b222627116d8
成都产研中心 刘美伶 hz18053297 <hz18053297@oa.com>
2019-11-12 11:42:47 +0800

    增加初始值

--- slide-ruler/index.js
+++ slide-ruler/index.js
@@ -1,114 +1,12 @@
 ;(function() {
-  let valueElement = document.querySelector('.js-value'),
-    rulerUlElement = document.querySelector('.js-ruler'),
-    rulerContainerElement = document.querySelector('.js-ruler-container'),
-    scaleElement = document.querySelector('.js-scale'),
-    scaleWidth = scaleElement.clientWidth,
-    translateX = 0,
-    pointX = 0,
-    startX = 0,
-    offsetX = 0,
-    startTime = new Date().getTime()
-
-  const min = 60,
-    max = 105,
-    maxTranslate = -((max - min) * scaleWidth),
-    deceleration = 0.0006
-
-  function scrollTo(x, time = 0, easing = 'cubic-bezier(0.1, 0.57, 0.1, 1)') {
-    rulerUlElement.style.transitionTimingFunction = easing
-    rulerUlElement.style.transitionDuration = `${time}ms`
-    translate(x)
-  }
-  function translate(x) {
-    let value = min - Math.round(x / scaleWidth)
-    if (value < min) {
-      value = min
-    } else if (value > max) {
-      value = max
+  let slideRuler = new SlideRuler({
+    container: document.querySelector(".js-ruler-container"),
+    max: 105,
+    min: 50,
+    value: 70,
+    callback(value) {
+      document.querySelector(".js-value").innerHTML = value;
     }
-    rulerUlElement.style.transform = `translateX(${x}px)`
-    rulerUlElement.style.webkitTransform = `translateX(${x}px)`
-    valueElement.innerHTML = value
-    translateX = x
-  }
-  function handleOutBoundary() {
-    if (translateX > 0 || translateX < maxTranslate) {
-      translateX = translateX > 0 ? 0: maxTranslate
-      scrollTo(translateX, 600)
-      return true
-    }
-  }
-  function startHandler(event) {
-    startTime = new Date().getTime()
-    offsetX = 0
-    startX = translateX
-    pointX = event.touches[0].pageX
-  }
-
-  function moveHandler(event) {
-    event.preventDefault()
-    // 实际滑动距离
-    offsetX = event.touches[0].pageX - pointX
-    pointX = event.touches[0].pageX
-    let x = translateX + offsetX
-    // 超出边界需要减速
-    if (x > 0 || x < maxTranslate) {
-      x = translateX + offsetX / 3
-    }
-    translate(x)
-  }
-
-  function endHandler(event) {
-    event.preventDefault()
-    let duration = new Date().getTime() - startTime,
-      easing = ''
-
-    // 超出边界值
-    if (handleOutBoundary()) {
-      return
-    }
-    if (translateX % scaleWidth !== 0) {
-      translateX = Math.round(translateX / scaleWidth) * scaleWidth
-    }
-    scrollTo(translateX)
-    if (duration < 300) {
-      let distance = translateX - startX,
-        speed = Math.abs(distance) / duration,
-        destination =
-          translateX +
-          ((speed * speed) / (2 * deceleration)) * (distance < 0 ? -1 : 1),
-        time = speed / deceleration
-      if (destination > 0) {
-        destination = (rulerContainerElement.clientWidth / 2.5) * (speed / 8)
-        distance = Math.abs(translateX) + destination
-        time = distance / speed
-      } else if (destination < maxTranslate) {
-        destination =
-          maxTranslate - (rulerContainerElement.clientWidth / 2.5) * (speed / 8)
-        distance = Math.abs(destination - translateX)
-        time = distance / speed
-      }
-      destination = Math.round(destination)
-      if (destination !== translateX) {
-        if (destination > 0 || destination < maxTranslate) {
-          easing = 'cubic-bezier(0.25, 0.46, 0.45, 0.94)'
-        }
-        if (destination % scaleWidth !== 0) {
-          destination = Math.round(destination / scaleWidth) * scaleWidth
-        }
-        scrollTo(destination, time, easing)
-      }
-    }
-  }
-
-  function transitionendHandler() {
-    rulerUlElement.style.transitionDuration = `0ms`
-    handleOutBoundary()
-  }
-  rulerUlElement.style.width = `${scaleWidth * (max - min)}px`
-  rulerUlElement.addEventListener('touchstart', startHandler)
-  rulerUlElement.addEventListener('touchmove', moveHandler)
-  rulerUlElement.addEventListener('touchend', endHandler)
-  rulerUlElement.addEventListener('transitionend', transitionendHandler)
-})()
+  });
+  slideRuler.init();
+})();
--- slide-ruler/slideRuler.js
+++ slide-ruler/slideRuler.js
@@ -1,166 +1,169 @@
 class SlideRuler {
-	constructor({
-		container,
-		max,
-		min,
-		value,
-		deceleration = 0.0006,
-		callback
-	}) {
-		this.container = container;
-		this.max = max;
-		this.min = min;
-		this.value = value || min;
-		this.deceleration = deceleration;
-		this.callback = callback;
-		this.translateX = 0;
-		this.pointX = 0;
-		this.startX = 0;
-		this.offsetX = 0;
-		this.maxTranslate = 0;
-		this.scaleWidth = 0;
-		this.startTime = new Date().getTime();
-	}
-	init() {
-		this.render();
-		this.regEvent();
-	}
-	render() {
-		this.ruler = this.getSliderRuler();
-		this.container.appendChild(this.ruler);
-		this.container.appendChild(this.getRulerMark());
-		this.scaleWidth = this.ruler.querySelector('.scale-item').clientWidth;
-		this.maxTranslate = -((this.max - this.min) * this.scaleWidth);
-		this.ruler.style.width = `${this.scaleWidth * (this.max - this.min)}px`;
-	}
-	regEvent() {
-		this.ruler.addEventListener('touchstart', this.startHandler);
-		this.ruler.addEventListener('touchmove', this.moveHandler)
-		this.ruler.addEventListener('touchend', this.endHandler)
-		this.ruler.addEventListener('transitionend', this.transitionendHandler)
-	}
-	getSliderRuler() {
-		let rulerElement = document.createElement('ul');
-		rulerElement.className = 'ruler-content';
-		for (let i = this.min; i <= this.max; i++) {
-			let scaleElement = document.createElement('li');
-			let numberElement = document.createElement('div');
-			scaleElement.className = 'scale-item';
-			numberElement.className = 'number';
-			scaleElement.appendChild(numberElement);
-			if (i % 5 === 0) {
-				let numberTextElement = document.createElement('div');
-				numberElement.className = 'number number-part';
-				numberTextElement.className = 'number-text';
-				numberTextElement.innerHTML = i;
-				scaleElement.appendChild(numberTextElement);
-			}
-			rulerElement.appendChild(scaleElement);
-		}
-		return rulerElement;
-	}
-	getRulerMark() {
-		let markElement = document.createElement('div');
-		markElement.className = 'mark';
-		return markElement;
-	}
-	translate(x) {
-		let value = this.min - Math.round(x / this.scaleWidth);
-		if (value < this.min) {
-			value = this.min;
-		} else if (value > this.max) {
-			value = this.max;
-		}
-		this.translateX = x;
-		this.value = value;
-		this.ruler.style.transform = `translateX(${x}px)`;
-		this.ruler.style.webkitTransform = `translateX(${x}px)`;
-		this.callback && this.callback(value);
-	}
-	scrollTo(x, time = 0, easing = 'cubic-bezier(0.1, 0.57, 0.1, 1)') {
-		this.ruler.style.transitionTimingFunction = easing;
-		this.ruler.style.transitionDuration = `${time}ms`;
-		this.translate(x);
-	}
-	isOutBoundary() {
-		if (this.translateX > 0 || this.translateX < this.maxTranslate) {
-			this.translateX = this.translateX > 0 ? 0 : this.maxTranslate;
-			console.log('isOutBoundary', this.translateX)
-			this.scrollTo(this.translateX, 600);
-			return true;
-		}
-	}
-	/**
-	 * 刻度尺滑动距离要自动吸附到最近的刻度上，不能在两个刻度中间
-	 */
-	roundTranslate(translateX) {
-		if (translateX % this.scaleWidth !== 0) {
-			return Math.round(translateX / this.scaleWidth) * this.scaleWidth;
-		}
-	}
-	inertiaSliding(time) {
-		let easing = '';
-		let distance = this.translateX - this.startX;
-		let speed = Math.abs(distance) / time;
-		let destination = this.translateX + ((speed * speed) / (2 * this.deceleration)) * (distance < 0 ? -1 : 1);
-		let duration = speed / this.deceleration;
-		if (destination > 0) {
-			destination = (this.container.clientWidth / 2.5) * (speed / 8);
-			distance = Math.abs(this.translateX) + destination;
-			duration = distance / speed;
-		} else if (destination < this.maxTranslate) {
-			destination = this.maxTranslate - (this.container.clientWidth / 2.5) * (speed / 8);
-			distance = Math.abs(destination - this.translateX);
-			duration = distance / speed;
-		}
-		destination = Math.round(destination);
-		// 如果有惯性滑动
-		if (destination !== this.translateX) {
-			// 惯性滑动超出了边界
-			if (destination > 0 || destination < this.maxTranslate) {
-				easing = 'cubic-bezier(0.25, 0.46, 0.45, 0.94)';
-			}
-			destination = this.roundTranslate(destination);
-			console.log('inertiaSliding', destination)
-			this.scrollTo(destination, duration, easing);
-		}
-	}
-	startHandler = event => {
-		let point = event.touches[0];
-		this.startTime = new Date().getTime();
-		this.offsetX = 0;
-		this.startX = this.translateX;
-		this.pointX = point.pageX;
-	}
-	moveHandler = event => {
-		event.preventDefault();
-		let point = event.touches[0];
-		this.offsetX = point.pageX - this.pointX;
-		this.pointX = point.pageX;
-		let x = this.translateX + this.offsetX;
-		// 超出边界需要减速
-		if (x > 0 || x < this.maxTranslate) {
-			x = this.translateX + this.offsetX / 3;
-		}
-		this.translate(x);
-	}
-	endHandler = event => {
-		event.preventDefault();
-		let duration = new Date().getTime() - this.startTime;
-		// 超出边界
-		if (this.isOutBoundary()) {
-			return;
-		}
-		this.translateX = this.roundTranslate(this.translateX);
-		console.log('endHandler', this.translateX)
-		this.scrollTo(this.translateX);
-		// 处理惯性滑动
-		if (duration < 300) {
-			this.inertiaSliding(duration);
-		}
-	}
-	transitionendHandler = () => {
-		this.ruler.transitionDuration = '0ms';
-		this.isOutBoundary();
-	}
+  constructor({ container, max, min, value, deceleration = 0.0006, callback }) {
+    this.container = container;
+    this.max = max;
+    this.min = min;
+    this.value = value || min;
+    this.deceleration = deceleration;
+    this.callback = callback;
+    this.translateX = 0;
+    this.pointX = 0;
+    this.startX = 0;
+    this.offsetX = 0;
+    this.maxTranslate = 0;
+    this.scaleWidth = 0;
+    this.startTime = new Date().getTime();
+  }
+  init() {
+    this.render();
+    this.regEvent();
+  }
+  renderInitValue() {
+    if (this.value > this.min && this.value < this.max) {
+      this.translate(-(this.value - this.min) * this.scaleWidth);
+    }
+  }
+  render() {
+    this.ruler = this.getSliderRuler();
+    this.container.appendChild(this.ruler);
+    this.container.appendChild(this.getRulerMark());
+    this.scaleWidth = this.ruler.querySelector(".scale-item").clientWidth;
+    this.maxTranslate = -((this.max - this.min) * this.scaleWidth);
+    this.ruler.style.width = `${this.scaleWidth * (this.max - this.min)}px`;
+    this.renderInitValue();
+  }
+  regEvent() {
+    this.ruler.addEventListener("touchstart", this.startHandler);
+    this.ruler.addEventListener("touchmove", this.moveHandler);
+    this.ruler.addEventListener("touchend", this.endHandler);
+    this.ruler.addEventListener("transitionend", this.transitionendHandler);
+  }
+  getSliderRuler() {
+    let rulerElement = document.createElement("ul");
+    rulerElement.className = "ruler-content";
+    for (let i = this.min; i <= this.max; i++) {
+      let scaleElement = document.createElement("li");
+      let numberElement = document.createElement("div");
+      scaleElement.className = "scale-item";
+      numberElement.className = "number";
+      scaleElement.appendChild(numberElement);
+      if (i % 5 === 0) {
+        let numberTextElement = document.createElement("div");
+        numberElement.className = "number number-part";
+        numberTextElement.className = "number-text";
+        numberTextElement.innerHTML = i;
+        scaleElement.appendChild(numberTextElement);
+      }
+      rulerElement.appendChild(scaleElement);
+    }
+    return rulerElement;
+  }
+  getRulerMark() {
+    let markElement = document.createElement("div");
+    markElement.className = "mark";
+    return markElement;
+  }
+  translate(x) {
+    let value = this.min - Math.round(x / this.scaleWidth);
+    if (value < this.min) {
+      value = this.min;
+    } else if (value > this.max) {
+      value = this.max;
+    }
+    this.translateX = x;
+    this.value = value;
+    this.ruler.style.transform = `translateX(${x}px)`;
+    this.ruler.style.webkitTransform = `translateX(${x}px)`;
+    this.callback && this.callback(this.value);
+  }
+  scrollTo(x, time = 0, easing = "cubic-bezier(0.1, 0.57, 0.1, 1)") {
+    this.ruler.style.transitionTimingFunction = easing;
+    this.ruler.style.transitionDuration = `${time}ms`;
+    this.translate(x);
+  }
+  isOutBoundary() {
+    if (this.translateX > 0 || this.translateX < this.maxTranslate) {
+      this.translateX = this.translateX > 0 ? 0 : this.maxTranslate;
+      console.log("isOutBoundary", this.translateX);
+      this.scrollTo(this.translateX, 600);
+      return true;
+    }
+  }
+  /**
+   * 刻度尺滑动距离要自动吸附到最近的刻度上，不能在两个刻度中间
+   */
+  roundTranslate(translateX) {
+    if (translateX % this.scaleWidth !== 0) {
+      return Math.round(translateX / this.scaleWidth) * this.scaleWidth;
+    }
+    return translateX;
+  }
+  inertiaSliding(time) {
+    let easing = "";
+    let distance = this.translateX - this.startX;
+    let speed = Math.abs(distance) / time;
+    let destination =
+      this.translateX +
+      ((speed * speed) / (2 * this.deceleration)) * (distance < 0 ? -1 : 1);
+    let duration = speed / this.deceleration;
+    if (destination > 0) {
+      destination = (this.container.clientWidth / 2.5) * (speed / 8);
+      distance = Math.abs(this.translateX) + destination;
+      duration = distance / speed;
+    } else if (destination < this.maxTranslate) {
+      destination =
+        this.maxTranslate - (this.container.clientWidth / 2.5) * (speed / 8);
+      distance = Math.abs(destination - this.translateX);
+      duration = distance / speed;
+    }
+    destination = Math.round(destination);
+    // 如果有惯性滑动
+    if (destination !== this.translateX) {
+      // 惯性滑动超出了边界
+      if (destination > 0 || destination < this.maxTranslate) {
+        easing = "cubic-bezier(0.25, 0.46, 0.45, 0.94)";
+      }
+      destination = this.roundTranslate(destination);
+      console.log("inertiaSliding", destination);
+      this.scrollTo(destination, duration, easing);
+    }
+  }
+  startHandler = event => {
+    let point = event.touches[0];
+    this.startTime = new Date().getTime();
+    this.offsetX = 0;
+    this.startX = this.translateX;
+    this.pointX = point.pageX;
+  };
+  moveHandler = event => {
+    event.preventDefault();
+    let point = event.touches[0];
+    this.offsetX = point.pageX - this.pointX;
+    this.pointX = point.pageX;
+    let x = this.translateX + this.offsetX;
+    // 超出边界需要减速
+    if (x > 0 || x < this.maxTranslate) {
+      x = this.translateX + this.offsetX / 3;
+    }
+    this.translate(x);
+  };
+  endHandler = event => {
+    event.preventDefault();
+    let duration = new Date().getTime() - this.startTime;
+    // 超出边界
+    if (this.isOutBoundary()) {
+      return;
+    }
+    this.translateX = this.roundTranslate(this.translateX);
+    console.log("endHandler", this.translateX);
+    this.scrollTo(this.translateX);
+    // 处理惯性滑动
+    if (duration < 300) {
+      this.inertiaSliding(duration);
+    }
+  };
+  transitionendHandler = () => {
+    this.ruler.transitionDuration = "0ms";
+    this.isOutBoundary();
+  };
 }
